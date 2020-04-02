10a44835b4327e00e21b934d8ffcf75ed4ab8638
zee <zeeshiq58@gmail.com>
2020-04-03 01:36:20 +0500

    add routes in route/api and make resource article

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//list Articles
+Route::get('articles','ArticleController@index');
+
+//list single article
+Route::get('article/{id}','ArticleController@show');
+// create new article
+Route::post('article','ArticleController@store');
+// update article
+Route::put('article','ArticleController@store');
+// delete article
+Route::delete('article','ArticleController@destroy');
